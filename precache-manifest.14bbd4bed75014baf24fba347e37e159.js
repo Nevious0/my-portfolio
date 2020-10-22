8cc9f3b994ed10cc35c20930615c61a88f8bcc0e
nevious <neviousndlovu@gmail.com>
2020-10-22 14:12:15 +0200

    Updates

---
+++ precache-manifest.14bbd4bed75014baf24fba347e37e159.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d85b2d5011ffa37af5ce518681a1b503",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "73e7a8d26b8ad71d1ddc",
+    "url": "/my-portfolio/static/css/main.6c6d19bc.chunk.css"
+  },
+  {
+    "revision": "ea2d0815c9ed0d0adcba",
+    "url": "/my-portfolio/static/js/2.12b92751.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-portfolio/static/js/2.12b92751.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73e7a8d26b8ad71d1ddc",
+    "url": "/my-portfolio/static/js/main.e9ac4a3a.chunk.js"
+  },
+  {
+    "revision": "e53dd08663c9f2b07f2a",
+    "url": "/my-portfolio/static/js/runtime-main.17605ec0.js"
+  }
+]);
